dc4b5607334273cf9c5953ddd36b37ad50323154
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-11-17 16:09:04 +0200

    Added sql table creation commands

---
+++ movie-service-table-creation.sql
@@ -0,0 +1,39 @@
+
+CREATE TABLE movie_genre (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    genre_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE movie_user (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    year_of_birth INT
+);
+
+CREATE TABLE movie (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(255) NOT NULL,
+    year INT NOT NULL,
+    genre_id INT NOT NULL,
+    FOREIGN KEY (genre_id) REFERENCES movie_genre(id)
+);
+
+CREATE TABLE favorite_movie (
+    user_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    PRIMARY KEY (user_id, movie_id),
+    FOREIGN KEY (user_id) REFERENCES movie_user(id),
+    FOREIGN KEY (movie_id) REFERENCES movie(id)
+);
+
+CREATE TABLE movie_review (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    stars INT CHECK (stars >= 1 AND stars <= 5),
+    review_text TEXT,
+    FOREIGN KEY (user_id) REFERENCES movie_user(id),
+    FOREIGN KEY (movie_id) REFERENCES movie(id)
+);
