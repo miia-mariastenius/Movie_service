5f06215697e8d2d044addafce136299037b07f2a
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-12-14 14:48:56 +0200

    Changed the DB connection to pooling and added connection js file

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import express from "express"
-import pg from "pg"
 import dotenv from "dotenv"
+import { pgPool } from "./pg_connection.js"
 
 dotenv.config()
-
-const {Client} = pg
 
 const app = express()
 
@@ -15,21 +13,6 @@ app.listen(3001, () => {
   console.log('Server is running!')
 })
 
-const client = new Client()
-
-connectDB()
-
-async function connectDB() {
-
-  try {
-    await client.connect()
-    console.log('Database connected...')
-    
-  } catch (error) {
-    console.log(error.message)
-  }
-  
-}
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Movie API')
@@ -38,7 +21,7 @@ app.get('/', (req, res) => {
 // Add a new genre
 app.post("/genres", async (req, res) => {
   const name = req.body.name
-  const result = await client.query(
+  const result = await pgPool.query(
     "INSERT INTO movie_genre (genre_name) VALUES ($1) RETURNING *",
     [name]
   )
@@ -48,7 +31,7 @@ app.post("/genres", async (req, res) => {
 // Add a new movie
 app.post("/movie", async (req, res) => {
   const { name, year, genreId } = req.body
-  const result = await client.query(
+  const result = await pgPool.query(
     "INSERT INTO movie (title, year, genre_id) VALUES ($1, $2, $3) RETURNING *",
     [name, year, genreId]
   )
@@ -58,7 +41,7 @@ app.post("/movie", async (req, res) => {
 // Register a new user
 app.post("/register", async (req, res) => {
   const { name, username, password, birthYear } = req.body
-  const result = await client.query(
+  const result = await pgPool.query(
     "INSERT INTO movie_user (name, username, password, year_of_birth) VALUES ($1, $2, $3, $4) RETURNING *",
     [name, username, password, birthYear]
   )
@@ -68,7 +51,7 @@ app.post("/register", async (req, res) => {
 // Get a movie by ID
 app.get("/movie/:id", async (req, res) => {
   const id = req.params.id
-  const result = await client.query("SELECT * FROM movie WHERE id = $1", [id])
+  const result = await pgPool.query("SELECT * FROM movie WHERE id = $1", [id])
   res.status(200).json(result.rows[0])
 })
 
@@ -76,7 +59,7 @@ app.get("/movie/:id", async (req, res) => {
 app.delete("/movie/:id", async (req, res) => {
   const id = req.params.id
   try {
-    await client.query('DELETE FROM movies WHERE id = $1', [id])
+    await pgPool.query('DELETE FROM movies WHERE id = $1', [id])
     res.status(200).send({ message: `Deleted movie with ID: ${id}` })
   } catch (error) {
     console.error(error.message)
@@ -86,14 +69,14 @@ app.delete("/movie/:id", async (req, res) => {
 
 // Get all movies
 app.get("/movies", async (req, res) => {
-  const result = await client.query("SELECT * FROM movie")
+  const result = await pgPool.query("SELECT * FROM movie")
   res.status(200).json(result.rows)
 })
 
 // Search movies by a keyword
 app.get("/movie", async (req, res) => {
   const keyword = req.query.keyword
-  const result = await client.query(
+  const result = await pgPool.query(
     "SELECT * FROM movie WHERE title ILIKE $1",
     [`%${keyword}%`]
   )
@@ -105,14 +88,14 @@ app.post("/review", async (req, res) => {
   const { username, movieId, stars, desc } = req.body
 
   // Find user ID by username
-  const userResult = await client.query(
+  const userResult = await pgPool.query(
     "SELECT id FROM movie_user WHERE username = $1",
     [username]
   )
   const userId = userResult.rows[0].id
 
   // Insert review into the database
-  const result = await client.query(
+  const result = await pgPool.query(
     "INSERT INTO movie_review (user_id, movie_id, stars, review_text) VALUES ($1, $2, $3, $4) RETURNING *",
     [userId, movieId, stars, desc]
   )
@@ -124,14 +107,14 @@ app.post("/favorite", async (req, res) => {
   const { username, movieId } = req.body
 
   // Find user ID by username
-  const userResult = await client.query(
+  const userResult = await pgPool.query(
     "SELECT id FROM movie_user WHERE username = $1",
     [username]
   )
   const userId = userResult.rows[0].id
 
   // Add favorite movie to the database
-  const result = await client.query(
+  const result = await pgPool.query(
     "INSERT INTO favorite_movie (user_id, movie_id) VALUES ($1, $2) RETURNING *",
     [userId, movieId]
   )
@@ -143,14 +126,14 @@ app.get("/favorites", async (req, res) => {
   const username = req.query.username
 
   // Find user ID by username
-  const userResult = await client.query(
+  const userResult = await pgPool.query(
     "SELECT id FROM movie_user WHERE username = $1",
     [username]
   )
   const userId = userResult.rows[0].id
 
   // Fetch favorite movies
-  const result = await client.query(
+  const result = await pgPool.query(
     "SELECT movie.* FROM favorite_movie JOIN movie ON favorite_movie.movie_id = movie.id WHERE favorite_movie.user_id = $1",
     [userId]
   )
